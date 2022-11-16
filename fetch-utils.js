9bf5bb7cdc63ee3f79dbd561992f3203a7e8f913
spiderforrest <spider@spood.org>
2022-11-16 14:50:29 -0800

    signUpUser()

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,7 @@
 // Enter Supabase Information
 const SUPABASE_URL = 'https://njqdydcjmajdjmyztzov.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcWR5ZGNqbWFqZGpteXp0em92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMjAsImV4cCI6MTk4MzY4NDAyMH0.r6bSNSp-6Ts4GRV3-pnwjFMUWdUGlWU4EiIWbDqrTXU';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcWR5ZGNqbWFqZGpteXp0em92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMjAsImV4cCI6MTk4MzY4NDAyMH0.r6bSNSp-6Ts4GRV3-pnwjFMUWdUGlWU4EiIWbDqrTXU';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,7 +9,13 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+// sign a user up
+export async function signupUser(email, password) {
+    // call the sign up api
+    const response = await client.auth.signUp({ email, password });
+    if (response.error) return response.error;
+    return response.user;
+}
 
 export async function signInUser(email, password) {}
 
