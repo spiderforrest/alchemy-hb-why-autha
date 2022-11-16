3c85d37d3faaec25bd95ab4cb284b7f33996fc6f
spiderforrest <spider@spood.org>
2022-11-16 15:24:25 -0800

    other-page.js

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,10 @@
+import { logout, checkAuth } from '../fetch-utils.js';
 // use checkAuth function to redirect is user not authenticated
+window.addEventListener('load', () => {
+    checkAuth();
+});
 // add event listener to the logout button and call logout
+const logoutButton = document.getElementById('logout');
+logoutButton.addEventListener('click', () => {
+    logout();
+});
